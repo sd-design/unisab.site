c16b5513b63446938f7f4e9d15531ba46b31fdf8
Alex Solovey <naghtigall@gmail.com>
2023-03-06 15:51:40 +0300

    Правки от 06.03.23
    https://docs.google.com/document/d/198IZFidx2N0Y3Zwe3-D1LJlKAm6ZAh2yCfVfEEwNTvU/edit

--- js/script.js
+++ js/script.js
@@ -2,13 +2,38 @@ var glide = new Glide('#advantageSlider', {
     type: 'carousel',
     perView: 4,
     breakpoints: {
-      1024: {
+      1160: {
         perView: 3
+      },
+      1024: {
+        perView: 2
+      },
+      800: {
+        perView: 2
       },
       576: {
         perView: 1
       }
     }
   })
-  
+
+  var glideNews = new Glide('#newsSlider', {
+    type: 'carousel',
+    perView: 3,
+    breakpoints: {
+      1160: {
+        perView: 3
+      },
+      1024: {
+        perView: 2
+      },
+      800: {
+        perView: 2
+      },
+      576: {
+        perView: 1
+      }
+    }
+  })
+  glideNews.mount()
   glide.mount()
